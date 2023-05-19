233929e121c63afebca0acb866e0069347731be8
Sean <seankim912@yahoo.com>
2023-05-18 22:16:51 -0500

    more code

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
 
-int x = 12 * 30;
-Console.WriteLine(x);
+Console.WriteLine("Hello, World!");
+
+// First int specifies type of return value, second int specifies type of the paramter being input
+
+int FeetToInches(int feet)
+{
+    int inches = feet * 12;
+    return inches;
+}
+
+Console.WriteLine(FeetToInches(10));
+
+// Void indicates no type is being returned.
+
+SayBye();
+
+void SayBye()
+{
+    Console.WriteLine("Bye");
+}
+
+/*
+Literals - primitive pieces of data lexically embedded into the program.
+*/
