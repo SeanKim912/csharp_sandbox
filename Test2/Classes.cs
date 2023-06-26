7eb55ed766800d5d3a279c0a06fc8c06bd02e883
Sean <seankim912@yahoo.com>
2023-06-25 23:06:00 -0500

    interfaces and stuff

---
+++ Test2/Classes.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace Basics
+{
+    /*
+    An interface is a group of interactions in C# that includes properties, methods,
+    and other members. Declared with a signature but bheaviors are not defined. A class
+    is said to implement an interface when it defines those properties and methods.
+    */
+    interface IAutomobile // An interface should always be named with a capital "I".
+    {
+        /*
+        You don't really care HOW the properties below work when it comes to the interface,
+        you just care that every instance of the related class HAS the properties below.
+        Interfaces provide another layer of code safety by catching errors early.
+
+        Interfaces cannot specify constructors or fields.
+        */
+        string LicensePlate { get; }
+        double Speed { get; }
+        int Wheels { get; }
+        void Honk();
+    }
+
+    class Sedan : IAutomobile
+    {
+        public string LicensePlate
+        {
+            get;
+            set;
+        }
+        public double Speed
+        {
+            get;
+            set;
+        }
+        public int Wheels
+        {
+            get;
+            set;
+        }
+        public void Honk()
+        {
+            Console.WriteLine("HONK");
+        }
+    }
+
+    // If code above did not properly implement IAutomobile interface, would throw error CS0535;
+}
--- Test2/Program.cs
+++ Test2/Program.cs
@@ -1,31 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Basics;
 
-/*
-To start a new C# program, create new project folder, then run
-"dotnet new console" in the terminal.
-*/
+    Sedan nissan = new Sedan();
+    nissan.LicensePlate = "AAAA";
+    nissan.Speed = 88.0;
+    nissan.Wheels = 4;
+    nissan.Honk();
 
-using System;
-
-Console.WriteLine("Butts");
-
-namespace Basics
-{
-    /*
-    An interface is a group of interactions in C# that includes properties, methods,
-    and other members. Declared with a signature but bheaviors are not defined. A class
-    is said to implement an interface when it defines those properties and methods.
-    */
-    interface IAutomobile // An interface should always be named with a capital "I".
-    {
-        /*
-        You don't really care HOW the properties below work when it comes to the interface,
-        you just care that every instance of the related class HAS the properties below.
-        Interfaces provide another layer of code safety by catching errors early.
-        */
-        string LicensePlate { get; }
-        double Speed { get; }
-        int Wheels { get; }
-        void Honk();
-    }
-}
+    Console.WriteLine(nissan.LicensePlate);
+    Console.WriteLine(nissan.Speed);
+    Console.WriteLine(nissan.Wheels);
